32986e2f7ce4aa0d4a051e9b4539cfbdb81d9533
sijegbulem-ch <sijegbulem@companieshouse.gov.uk>
2025-12-18 17:43:51 +0000

    tfupgrade

--- groups/chips-uam-infrastructure/main.tf
+++ groups/chips-uam-infrastructure/main.tf
@@ -3,7 +3,7 @@
 # Providers
 # ------------------------------------------------------------------------------
 terraform {
-  required_version = "1.3.0, < 2.0.0"
+  required_version = ">= 1.3.0, < 2.0.0"
 
   required_providers {
     aws = {
